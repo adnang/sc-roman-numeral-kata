296826bb457923720c31d3ed60ed71d8a65fd170
Adnan Gazi <adnan.gazi@asos.com>
2017-01-11 15:27:15 +0000

    returns I for 1

---
+++ RomanNumeralKata.Test/RomanNumeralCalculatorShould.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace RomanNumeralKata.Test
+{
+    [TestFixture]
+    public class RomanNumeralCalculatorShould
+    {
+        [TestCase(1, "I")]
+        public void ReturnRomanNumeral_WhenCalculatingRomanNumeral_GivenArabicNumber(int arabicNumber, string expectedRomanNumeral)
+        {
+            RomanNumeralCalculator romanNumeralCalculator = new RomanNumeralCalculator();
+            string romanNumeral = romanNumeralCalculator.Calculate(arabicNumber);
+            romanNumeral.Should().Be(expectedRomanNumeral);
+        }
+    }
+
+    public class RomanNumeralCalculator
+    {
+        public string Calculate(int arabicNumber)
+        {
+            return "I";
+        }
+    }
+}
