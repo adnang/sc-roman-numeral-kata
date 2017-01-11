e3ade2a6d53562fd210f113c97952ad4b0a0eac7
Adnan Gazi <adnan.gazi@asos.com>
2017-01-11 16:07:33 +0000

    refactor to add recursion

--- RomanNumeralKata/RomanNumeralCalculator.cs
+++ RomanNumeralKata/RomanNumeralCalculator.cs
@@ -5,8 +5,9 @@ namespace RomanNumeralKata
 {
     public class RomanNumeralCalculator
     {
-        private readonly Dictionary<int, string> romanBy = new Dictionary<int, string>() 
+        private readonly Dictionary<int, string> romanBy = new Dictionary<int, string>()
         {
+            {1, "I"},
             {4, "IV"},
             {5, "V"},
             {6, "VI"},
@@ -17,12 +18,27 @@ namespace RomanNumeralKata
 
         public string Calculate(int arabicNumber)
         {
-            if (arabicNumber < 4)
+            if (arabicNumber >= 9)
             {
-                return new string('I', arabicNumber);
+                return "IX";
             }
 
-            return romanBy[arabicNumber];
+            if (arabicNumber >= 5)
+            {
+                return "V" + Calculate(arabicNumber - 5);
+            }
+
+            if (arabicNumber == 4)
+            {
+                return "IV" + Calculate(arabicNumber - 4);
+            }
+
+            if (arabicNumber >= 1)
+            {
+                return "I" + Calculate(arabicNumber - 1);
+            }
+
+            return string.Empty;
         }
     }
 }
