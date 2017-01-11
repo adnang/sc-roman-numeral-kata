c65aef3a7d23d06e380dbf354ac4efd8064d5c2f
Adnan Gazi <adnan.gazi@asos.com>
2017-01-11 16:26:14 +0000

    returns L for 50

--- RomanNumeralKata.Test/RomanNumeralCalculatorShould.cs
+++ RomanNumeralKata.Test/RomanNumeralCalculatorShould.cs
@@ -16,6 +16,10 @@ namespace RomanNumeralKata.Test
         [TestCase(8, "VIII")]
         [TestCase(9, "IX")]
         [TestCase(10, "X")]
+        [TestCase(11, "XI")]
+        [TestCase(20, "XX")]
+        [TestCase(18, "XVIII")]
+        [TestCase(50, "L")]
         public void ReturnRomanNumeral_WhenCalculatingRomanNumeral_GivenArabicNumber(int arabicNumber, string expectedRomanNumeral)
         {
             var romanNumeralCalculator = new RomanNumeralCalculator();
--- RomanNumeralKata/RomanNumeralCalculator.cs
+++ RomanNumeralKata/RomanNumeralCalculator.cs
@@ -7,6 +7,7 @@ namespace RomanNumeralKata
     {
         private readonly Dictionary<int, string> arabicToRoman = new Dictionary<int, string>
         {
+            { 50, "L" },
             { 10, "X" },
             { 9, "IX" },
             { 5, "V" },
